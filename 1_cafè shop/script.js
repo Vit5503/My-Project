914783bdfc6800e72675f258fe677ae90b19071b
4Teen <fteen4851>
2026-01-06 11:32:30 +0700

    updated 06012026

--- 1_cafè shop/script.js
+++ 1_cafè shop/script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -7,7 +8,12 @@ menuOpenButton.addEventListener("click", () => {
 });
 
 //Close menu when the close button is clicked
-menuCloseButton.addEventListener("click", () => menuOpenButton.click ());
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+//Close menu when the close button is clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", () => menuOpenButton.click())
+});
 
 // Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
