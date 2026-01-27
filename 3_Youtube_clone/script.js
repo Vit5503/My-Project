dd30e37ed2c67032f6cfd63e2f4691d5cafaeebb
4Teen <fteen4851>
2026-01-27 08:25:10 +0700

    updated d27m1y26

---
+++ 3_Youtube_clone/script.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelectorAll(".menu-button");
+
+// Toggle sidebar visibility when menu buttons are clicked
+menuButton.forEach(button => {
+    button.addEventListener("click", () => {
+        document.body.classList.toggle("sidebar-hidden");
+    });  
+});
