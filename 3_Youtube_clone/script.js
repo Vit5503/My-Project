8a0365d577ecc71d37e6f36d3b988fd3b6c2eb52
4Teen <fteen4851>
2026-01-27 11:02:37 +0700

    d27m1y26

--- 3_Youtube_clone/script.js
+++ 3_Youtube_clone/script.js
@@ -1,4 +1,22 @@
 const menuButton = document.querySelectorAll(".menu-button");
+const screenOverlay = document.querySelector(".screen-overlay");
+const themeButton = document.querySelector(".theme-button i");
+
+// initailize dark mode based on localstorage
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    themeButton.classList.replace("fa-moon", "fa-sun");
+}else{
+    themeButton.classList.replace("fa-sun", "fa-moon");
+}
+
+// toogle dark mode when theme button is click
+themeButton.addEventListener("click", () => {
+    const isDarkMode = document.body.classList.toggle("dark-mode");
+    localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+    themeButton.classList.toggle("fa-sun", isDarkMode);
+    themeButton.classList.toggle("fa-moon", !isDarkMode);
+});
 
 // Toggle sidebar visibility when menu buttons are clicked
 menuButton.forEach(button => {
@@ -6,3 +24,13 @@ menuButton.forEach(button => {
         document.body.classList.toggle("sidebar-hidden");
     });  
 });
+
+// Toggle sidebar visibility when screen overlay is clicked
+screenOverlay.addEventListener("click", () => {
+    document.body.classList.toggle("sidebar-hidden");
+}); 
+
+// Show sidebar on large screens by default
+if (window.innerWidth >= 768) {
+    document.body.classList.remove("sidebar-hidden");
+};
